1623b6db8266f5e0625a08a0e188dd5009ca0504
tatitrif <64037685+tatitrif@users.noreply.github.com>
2022-10-27 14:04:00 +0300

    fix env

--- notification-service/admin/src/config/components/database.py
+++ notification-service/admin/src/config/components/database.py
@@ -1,11 +1,11 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'movies_database'),
-        'USER': os.environ.get('DB_USER', 'app'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', '123qwe'),
-        'HOST': os.environ.get('DB_HOST', '62.84.115.122'),
-        'PORT': os.environ.get('DB_PORT', 5432),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
         'OPTIONS': {
             'options': '-c search_path=public,notify'
         }
